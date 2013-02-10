85e3a47233114700aa1ad2cb8620d49a09ea1f52
Petricioiu Robert <cpetricioiuster@gmail.com>
2013-02-10 15:03:22 +0200

    Initial Commit

---
+++ wordy/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  wordy
+//
+//  Created by Petricioiu Robert on 2/10/13.
+//  Copyright (c) 2013 Petricioiu Robert. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
